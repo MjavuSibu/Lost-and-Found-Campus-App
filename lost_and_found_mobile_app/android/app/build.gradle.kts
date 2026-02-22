bcbbe30666ce9c81fe359b5f29f675425154693b
Sibu Mjavu <mjavusibulele@gmail.com>
2026-02-22 14:26:55 +0200

    .

--- lost_and_found_mobile_app/android/app/build.gradle.kts
+++ lost_and_found_mobile_app/android/app/build.gradle.kts
@@ -12,14 +12,13 @@ android {
     namespace = "com.example.lost_and_found_mobile_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
